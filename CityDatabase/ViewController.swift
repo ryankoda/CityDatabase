b349ae3700cf05abdc27904f65d4a0a47d0ba179
RYAN KODA <ryankoda@outlook.com>
2019-01-06 15:31:12 -0800

    Initial Commit

---
+++ CityDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityDatabase
+//
+//  Created by Ryan Koda on 1/6/19.
+//  Copyright © 2019 Ryan Koda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
