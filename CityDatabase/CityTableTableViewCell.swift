93cf28bc0cfc72a0743b320024f428d7b06a4c36
RYAN KODA <ryankoda@outlook.com>
2019-01-06 15:48:31 -0800

    TableViewCell File

---
+++ CityDatabase/CityTableTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CityTableTableViewCell.swift
+//  CityDatabase
+//
+//  Created by Ryan Koda on 1/6/19.
+//  Copyright © 2019 Ryan Koda. All rights reserved.
+//
+
+import UIKit
+
+class CityTableTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
