0b33cb61a8d8ed236091769273327932edbf58eb
RYAN KODA <ryankoda@outlook.com>
2019-01-06 16:47:18 -0800

    Choose Picture

--- CityDatabase/CityViewController.swift
+++ CityDatabase/CityViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
+import CoreData
 
-class CityViewController: UIViewController, UIImagePickerControllerDelegate
+class CityViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
     var y:City?
     var tempName:String?
@@ -8,8 +9,48 @@ class CityViewController: UIViewController, UIImagePickerControllerDelegate
     var tempImage:UIImage?
     var tempRow:Int?
     
+    @IBOutlet weak var cityName: UILabel!
+    @IBOutlet weak var cityImage: UIImageView!
+    @IBOutlet weak var cityDescription: UILabel!
+    @IBOutlet weak var imageSource: UISegmentedControl!
+    
+    let picker = UIImagePickerController()
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        picker.delegate = self
+        self.cityName.text = self.tempName
+        self.cityDescription.text = self.tempDescription
+        self.cityImage.image = self.tempImage
+    }
+    @IBAction func ChoosePicture(_ sender: Any)
+    {
+        if imageSource.selectedSegmentIndex == 0
+        {
+            if UIImagePickerController.isSourceTypeAvailable(.camera)
+            {
+                picker.allowsEditing = false
+                picker.sourceType = UIImagePickerController.SourceType.camera
+                picker.cameraCaptureMode = .photo
+                picker.modalPresentationStyle = .fullScreen
+                present(picker, animated:true, completion:nil)
+            }
+        }
+        else
+        {
+            picker.allowsEditing = false
+            picker.sourceType = .photoLibrary
+            picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            picker.modalPresentationStyle = .popover
+            present(picker, animated:true, completion:nil)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        picker .dismiss(animated:true, completion:nil)
+        cityImage.image=info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        let imageData: NSData = cityImage.image!.pngData() as! NSData
+        y!.cityImage = imageData
     }
 }
